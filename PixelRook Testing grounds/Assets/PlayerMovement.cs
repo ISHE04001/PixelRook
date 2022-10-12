b8b124f1a4fe5d20f898cc930313d949e491a949
ISHE04001 <77394101+ISHE04001@users.noreply.github.com>
2022-10-12 12:31:55 +0200

    Base

---
+++ PixelRook Testing grounds/Assets/PlayerMovement.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    public float jumpForce;
+    public float speed;
+
+    Rigidbody2D rb;
+
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    public float rememberGroundedFor;
+    float lastTimeGrounded;
+
+    public int defaultAdditionalJumps = 1;
+    int additionalJumps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //låter koden använda sig av rigibody, speciellt dens gravitet.
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+        BetterJump();
+
+
+
+    }
+    void Move()
+    {
+        //´"Horizontal låter oss lösa "a" och "d" tangenterna tillsammans. "a" har ett värde av -1 och "d" har ett värde av 1. Om ingen trycks ner så är x = 0. 
+        float x = Input.GetAxisRaw("Horizontal");
+        //förklarar bara moveBy
+        float moveBy = x * speed;
+        //Denna del är den som rör förflyttear spelaren
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || additionalJumps > 0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            additionalJumps--;
+        }
+    }
+    void CheckIfGrounded()
+    {
+        Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (colliders != null)
+        {
+            isGrounded = true;
+            additionalJumps = defaultAdditionalJumps;
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
+
+
