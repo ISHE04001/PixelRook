b32e89b083e17e506e71d19de0bb578f2877c16e
ISHE04001 <77394101+ISHE04001@users.noreply.github.com>
2022-10-26 13:28:33 +0200

    EnemyCode

---
+++ PixelRook Testing grounds/Assets/EnemyEyes.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEyes : MonoBehaviour
+{
+
+    public float distance;
+    public LineRenderer LineOfSight;
+    public Gradient redColor;
+    public Gradient greenColor;
+    private void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+    }
+    void Update()
+    {
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            LineOfSight.colorGradient = redColor;
+
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                Destroy(hitInfo.collider.gameObject);
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+                LineOfSight.colorGradient = greenColor;
+            }
+
+        }
+    }
+}
+
